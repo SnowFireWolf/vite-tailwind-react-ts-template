ea89144ae3d79200a7a916ae9519d7350777fa67
SnowFireWolf <4399kevin11@gmail.com>
2022-04-25 11:41:20 +0800

    update for tailwind

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import tw from '@/styles/tailwind.module.css';
 
 
 
@@ -8,15 +9,16 @@ export default function BaseLayout({ ...props }) {
 
   return (
     <React.Fragment>
-      <p>
-        <button type="button" onClick={() => navigate('/')}>
-          Home
-        </button>
-        {' | '}
-        <button type="button" onClick={() => navigate('/about')}>
-          About
-        </button>
-      </p>
+      <div className={`${tw['w-full']} ${tw['bg-white']} ${tw['shadow-lg']} ${tw['py-4']}`}>
+        <div className={`${tw['flex']} ${tw['items-center']} ${tw['md:order-2']}`}>
+          <button className={`${tw['py-2']} ${tw['pr-4']} ${tw['pl-3']}`} type="button" onClick={() => navigate('/')}>
+            Home
+          </button>
+          <button className={`${tw['py-2']} ${tw['pr-4']} ${tw['pl-3']}`} type="button" onClick={() => navigate('/about')}>
+            About
+          </button>
+        </div>
+      </div>
       {props.children}
     </React.Fragment>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '@/assets/logo.svg'
-import './Home.css'
+import tw from '@/styles/tailwind.module.css'
 
 
 
@@ -8,38 +8,38 @@ export default function HomePage() {
   const [count, setCount] = React.useState<number>(0)
 
   return (
-    <React.Fragment>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React + React Router!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>./src/pages/Home.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </React.Fragment>
+    <div className={`${tw['container']} ${tw['mx-auto']}`}>
+      <img className={`${tw['w-1/4']}`} src={logo} alt="logo" />
+      <p>Hello Vite + React + React Router!</p>
+      <p>
+        <button
+          className={`${tw['mt-5']} ${tw['flex']} ${tw['flex-row']} ${tw['justify-center']} ${tw['items-center']} ${tw['p-3']} ${tw['bg-indigo-900']} ${tw['text-white']} ${tw['rounded-lg']} ${tw['shadow-lg']} ${tw['focus:outline-none']} ${tw['active:bg-indigo-800']} ${tw['disabled:opacity-80']}`}
+          type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
+      <p>
+        Edit <code>./src/pages/Home.jsx</code> and save to test HMR updates.
+      </p>
+      <p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {' | '}
+        <a
+          className="App-link"
+          href="https://vitejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vite Docs
+        </a>
+      </p>
+    </div>
   )
 }
